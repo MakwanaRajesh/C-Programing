83f1333b9696d1e2496173b781a50cba39d22b08
MakwanaRajesh <149988396+MakwanaRajesh@users.noreply.github.com>
2023-11-06 09:18:43 +0530

    Add files via upload

---
+++ 19A-2.c
@@ -0,0 +1,12 @@
+//2. Pass an array in function to print array elements.
+
+#include<stdio.h>
+
+void display(int a,int b){
+    printf("%d\n", a);
+    printf("%d\n", b);
+}
+void main(){
+    int elements[]={1,3,5,7,9};
+    display(elements[%d],(elements[%d])),a,b;
+}
